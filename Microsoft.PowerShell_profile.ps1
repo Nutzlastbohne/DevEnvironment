27a947e963032a14cef330360705ece127c31dc9
Robert Radestock <robert.radestock@affinitas.de>
2015-10-23 16:19:57 +0200

    updated ps scripts: major additions to custom PS-Environment - created variables for some app-paths - improved comments - added code to raise ConEmu thread priority to normal (default: minor) - etc.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,33 +21,53 @@ function appendMessage {
 	$global:message += "$args`n"
 }
 
+## global constants ##
+$NOTEPAD_HOME = "C:\Tools\Notepad++\notepad++.exe"
+$JBOSS_HOME = "C:\Dev\server\jboss-eap-6.2"
+$GIT_PATH = @(Get-Command git)[0]	# in case /cmd and /bin are listed => use first entry
+$GIT_HOME = (get-item $GIT_PATH.Definition).Directory.Parent.FullName
+
+######################################### ENV_VARS ######################################### 
+#appendMessage("--- add env_vars ---")
+#$env:MVN_DEFAULT="$HOME\.m2\settings.xml"
+
 ######################################### Aliases ######################################### 
-appendMessage("--- add aliases ---")
-Set-Alias np "D:\Programme\Notepad++\notepad++.exe"
-Set-Alias edit np
+appendMessage("--- add aliases & functions ---")
+appendMessage("`t edit FILE - open FILE with Notepad")
+Set-Alias edit $NOTEPAD_HOME
+appendMessage("`t find - Linux style 'find'")
+Set-Alias -Name find -Value $GIT_HOME\bin\find.exe -Option AllScope	# overwrite windows 'find' with Linux derivative
+#appendMessage("`t ll - alias for 'ls'")
 Set-Alias ll ls
+appendMessage("`t np FILE - alias for 'edit'")
+Set-Alias np edit
+appendMessage("`t which COMMAND - alias for 'Get-Command'")
 Set-Alias which Get-Command
 
+
 ######################################### Functions ######################################### 
-appendMessage("--- add functions ---")
 $oldErrorActionPreferente = $ErrorActionPreference
 $ErrorActionPreference = 'stop'								# All exceptions will be critical and step into the catch-block
 
-appendMessage("`t overwerite windows-style 'man' with linux style 'man'")
+appendMessage("`t jbcli - starts jboss console inside the current shell and renames it")
+function jbcli {
+	$ui.WindowTitle="JB-CLI"
+	start $JBOSS_HOME\bin\jboss-cli.bat
+}
+
+appendMessage("`t man COMMAND - linux style 'man'")
 function linuxStyleMan {
 	Get-Help "$args" -full | less							# windows default pager is lame... use less
 }
 
-Set-Alias -Name man -Value linuxStyleMan -Option AllScope	# overwrite default 'man'
+Set-Alias -Name man -Value linuxStyleMan -Option AllScope				# overwrite default 'man'
 
-appendMessage("`t add jbcli - starts jboss console and renames current shell")
-function jbcli {
-	$ui.WindowTitle="JB-CLI"
-	C:\Dev\server\jboss-as-7.1.3.Final\bin\jboss-cli.bat
+appendMessage("`t rc STRING - rename console")
+function rc {
+	$ui.WindowTitle=$args
 }
 
-
-appendMessage("`t testFiles - given a list of files, checks for existence")
+appendMessage("`t testFiles FILES - prints out which of the given FILES exist/don't exist")
 function testFiles {
 	foreach ($i in $args) { 
 		if (Test-Path $i) { 
@@ -58,7 +78,57 @@ function testFiles {
 	}
 }
 
+appendMessage("`t vim FILE - you know what that is...")
+function vim {
+	bash $GIT_HOME/bin/vim @args
+}
+
+######################################### Maven Stuff
+
+appendMessage("- add maven tooling -")
+appendMessage("`t mvn-ci [OPTIONS] - run maven clean install in failFast-Mode without tests")
+function mvn-ci {
+	mvn -ff clean install -DskipTests @args
+}
+
+appendMessage("`t mvn-cit [OPTIONS] - like mvn-ci, but with 8 Threads")
+function mvn-cit {
+	mvn -T 8 -ff clean install -DskipTests @args
+}
+
+
+######################################### Git Stuff
+Try {
+	appendMessage("- add git tooling -")
+	
+	Set-Alias gitk "$GIT_HOME\cmd\gitk.cmd"					# in case /cmd is not part of $PATH, link gitk manually
+
+	appendMessage("`t gdiff [COMMITS] [FILES] - git difftool -y")
+	function gdiff() {
+		git difftool -y @args
+	}
+	
+	appendMessage("`t gitg - git staging gui")
+	function gitg {
+		start $GIT_HOME\bin\wish.exe "$GIT_HOME\libexec\git-core\git-gui"
+	}
+	
+	appendMessage("`t gmerge [FILES] - git mergetool -y")
+	function gmerge() {
+		git mergetool -y @args
+	}
+} Catch [System.Management.Automation.CommandNotFoundException] {
+	appendMessage("`tGit doesnt seem to be installed - skipping related functions")
+}
+
+######################################### Load additional scripts ######################################### 
+appendMessage("--- add shell extensions ---")
+appendMessage("`tadd posh-git for great justice!")
+# Load posh-git example profile
+. "$HOME\git\posh-git\profile.example.ps1"
+
 ######################################### ConEmu
+# Set priority of each instance to 'normal'. May increase performance because it starts with 'low'
 # get all instances of ConEmu64
 function getEmu {
 	gwmi win32_process -f "name='ConEmu64.exe'"
@@ -73,37 +143,6 @@ function showEmuPrio {
 if ((getEmu) -ne $NULL){
 	@(getEmu) | foreach {$_.setPriority(32)}				# set priority of each instance to 'Normal'
 }
-
-######################################### Git Stuff
-Try {
-	appendMessage("- initializing git tooling -")
-	$GIT_PATH = @(which git)[0]	# in case /cmd and /bin are listed => use first entry
-	$GIT_HOME = (get-item $GIT_PATH.Definition).Directory.Parent.FullName
-	
-	# Set-Alias gitk "$GIT_HOME\cmd\gitk.cmd"					# in case /cmd is not part of $PATH, link gitk manually
-	
-	appendMessage("`tadd gitg - git staging gui")
-	function gitg {
-		start $GIT_HOME\bin\wish.exe "$GIT_HOME\libexec\git-core\git-gui"
-	}
-
-	appendMessage("`tadd gdiff - git difftool -y")
-	function gdiff() {
-		git difftool -y @args
-	}
-	
-	appendMessage("`tadd gmerge - git mergetool -y")
-	function gmerge() {
-		git mergetool -y @args
-	}
-} Catch [System.Management.Automation.CommandNotFoundException] {
-	appendMessage("`tGit doesnt seem to be installed - skipping related functions")
-}
-
-######################################### Load additional scripts ######################################### 
-
-# Load posh-git example profile
-. "$HOME\git\posh-git\profile.example.ps1"
 
 ######################################### Cleanup ######################################### 
 # clear screen after all this mess
--- shellInit.ps1
+++ shellInit.ps1
@@ -1,5 +1,5 @@
 # generate PowerShell Profile under %USER%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 New-Item -path $profile -type file –force
 
-# Allow Scripts to be executed
+# Allow Scripts to be executed (must be executed from PS (started as Admin))
 set-executionpolicy remotesigned
